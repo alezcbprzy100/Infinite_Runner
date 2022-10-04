022ef550a6f1afac58375a07d3e276c06bb5ddbc
alezcbprzy100 <scorpus3k@gmail.com>
2022-10-04 20:01:24 +0200

    Movement and camera follow

---
+++ Infinity Runner/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
+    private void FixedUpdate () 
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
